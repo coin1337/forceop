2bd9519718022a15c79b13c6b74a60296c791468
CHRISTALLiNQQ <66845682+christallinqq@users.noreply.github.com>
2021-06-13 21:22:11 -0400

    Update ForceOP.java

--- xyz/christallinqq/forceop/ForceOP.java
+++ xyz/christallinqq/forceop/ForceOP.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class FOE implements ModInitializer {
+public class ForceOP implements ModInitializer {
    public static Logger LOGGER = LogManager.getLogger();
    public static final String MOD_ID = "forceop";
    public static final String MOD_NAME = "Force OP exploit";
